621e314fa327c8cfa46a49c555b06e7c363be748
Lrdsnow <Lrdsnow101@gmail.com>
2024-12-25 12:08:05 -0800

    actually works on windows now

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 - build and install SimpleBLE from https://github.com/OpenBluetoothToolbox/SimpleBLE
 - run `make lib` to build a library
 - enjoy!
+
+### how to use: (windows)
+- install vs build tools from [here](https://visualstudio.microsoft.com/downloads)
+- place simpleble shared into `lib` from [here](https://github.com/OpenBluetoothToolbox/SimpleBLE/releases)
+- open a shell in the sdk folder
+- run `build`
+- enjoy!
--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-cl /c src/anki_sdk/protocol.c src/BluetoothManager.cpp src/helpers.cpp src/main_debug.cpp src/Track.cpp src/VehicleDelegate.cpp /I"C:\Program Files (x86)\simpleble\include" /std:c++17
-link /OUT:build/bin/PureDrive.exe protocol.obj BluetoothManager.obj helpers.obj main_debug.obj Track.obj VehicleDelegate.obj /link /LIBPATH:"C:\Program Files (x86)\simpleble\lib" simpleble-debug.lib simpleble-c-debug.lib
+mkdir build
+cl /c src/anki_sdk/protocol.c src/BluetoothManager.cpp src/helpers.cpp src/main_debug.cpp src/Track.cpp src/VehicleDelegate.cpp /Ilib\include /std:c++17
+link /OUT:build/PureDrive.exe protocol.obj BluetoothManager.obj helpers.obj main_debug.obj Track.obj VehicleDelegate.obj /link /LIBPATH:lib\lib simpleble.lib simpleble-c.lib
 del /Q *.obj
+copy lib\bin\* build
